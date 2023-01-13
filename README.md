5da4b3cab71d6befadb367bcb982f692006f4433
nidazhong <nidazhong17@163.com>
2023-01-14 02:02:16 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# 麦子商城Mini版
+
+## 依赖版本
+
+| soft                 | version       |
+|----------------------|---------------|
+| Java                 | 17            |
+| Maven                | 3.6.3         |
+| Spring Boot          | 2.7.7.RELEASE |
+| Mysql                | 8             |
---
+++ src/test/java/com/ndz/wheatmall/WheatmallMiniApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ndz.wheatmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * springboot 2.7 使用了junit5
+ */
+@SpringBootTest
+public class WheatmallMiniApplicationTests {
+    @Test
+    public void test01 () {
+        System.out.println("hello world");
+    }
+}
