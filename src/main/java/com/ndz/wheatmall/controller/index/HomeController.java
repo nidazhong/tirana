3876c0cc58f46881c9650c1dde1b11f5ec221efc
nidazhong <nidazhong17@163.com>
2023-01-14 03:56:42 +0800

    up log日志记录重复问题

--- src/main/java/com/ndz/wheatmall/controller/index/HomeController.java
+++ src/main/java/com/ndz/wheatmall/controller/index/HomeController.java
@@ -1,13 +1,23 @@
-package com.ndz.wheatmall.controller;
+package com.ndz.wheatmall.controller.index;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HomeController {
 
     @GetMapping("/hello")
     public String hello() {
+        log.debug("debug");
+        log.info("info");
+        log.warn("warn");
+        log.error("error");
         return "helloWorld";
     }
+
+
+
+
 }
