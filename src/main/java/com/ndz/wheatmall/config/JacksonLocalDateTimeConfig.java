fe3fade1e79c0b1b582e80cc6fc78835c8edff35
nidazhong <nidazhong17@163.com>
2023-01-14 16:00:38 +0800

    add LocalDateTime jackson优化

--- src/main/java/com/ndz/wheatmall/config/JacksonLocalDateTimeConfig.java
+++ src/main/java/com/ndz/wheatmall/config/JacksonLocalDateTimeConfig.java
@@ -2,17 +2,17 @@ package com.ndz.wheatmall.config;
 
 
 import java.io.IOException;
-import java.text.ParseException;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
+import java.util.Objects;
 
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.Formatter;
+import org.springframework.util.StringUtils;
 
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParser;
@@ -21,40 +21,16 @@ import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 
+import io.reactivex.rxjava3.annotations.Nullable;
+
 /**
  * 实现jackson对于LocalDate、LocalDateTime、LocalTime类型数据的格式化
  * 此类为全局默认配置，如类还想自定义则用@JsonFormat注解
  */
 @Configuration
 public class JacksonLocalDateTimeConfig {
-
-    /**
-     * LocalDate、LocalDateTime、LocalTime 的序列化和反序列化
-     */
-//    @Bean
-//    public ObjectMapper objectMapper() {
-//        ObjectMapper objectMapper = new ObjectMapper();
-//
-//        // other serializer and deSerializer config ...
-//
-//        JavaTimeModule javaTimeModule = new JavaTimeModule();
-//
-//        // timestamp
-//        javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
-//        javaTimeModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
-//
-//        // date
-//        javaTimeModule.addSerializer(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
-//        javaTimeModule.addDeserializer(LocalDate.class, new LocalDateDeserializer(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
-//
-//        // time
-//        javaTimeModule.addSerializer(LocalTime.class, new LocalTimeSerializer(DateTimeFormatter.ofPattern("HH:mm:ss")));
-//        javaTimeModule.addDeserializer(LocalTime.class, new LocalTimeDeserializer(DateTimeFormatter.ofPattern("HH:mm:ss")));
-//
-//        objectMapper.registerModule(javaTimeModule);
-//        return objectMapper;
-//    }
-
+    
+    public static final String DATETIME_PATTERN = "yyyy-MM-dd HH:mm:ss";
 
     /**
      * 适配自定义序列化和反序列化策略
@@ -63,6 +39,7 @@ public class JacksonLocalDateTimeConfig {
     public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
         return builder -> {
             // 序列化给前端不用时间戳， 如果返回前端也要时间戳则自己实现
+            // LocalDateTime
             builder.serializerByType(LocalDateTime.class, new LocalDateTimeSerializer());
             builder.deserializerByType(LocalDateTime.class, new LocalDateTimeDeserializer());
             // 如有其他类型，以下还可添加
@@ -77,7 +54,7 @@ public class JacksonLocalDateTimeConfig {
         public void serialize(LocalDateTime localDateTime, JsonGenerator jsonGenerator, SerializerProvider serializers)
                 throws IOException {
             if (localDateTime != null) {
-                String dateTime = localDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+                String dateTime = localDateTime.format(DateTimeFormatter.ofPattern(DATETIME_PATTERN));
                 jsonGenerator.writeString(dateTime);
                 // 时间戳格式输出
 //              jsonGenerator.writeNumber(localDateTime.toInstant(ZoneOffset.ofHours(8)).toEpochMilli());
@@ -110,20 +87,23 @@ public class JacksonLocalDateTimeConfig {
     @Bean
     public Formatter<LocalDateTime> localDateTimeFormatter() {
         return new Formatter<>() {
+            @Nullable
             @Override
-            public LocalDateTime parse(String text, Locale locale) throws ParseException {
-                return LocalDateTime.parse(text, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+            public LocalDateTime parse(@Nullable String text, @Nullable Locale locale) {
+                if (!StringUtils.hasText(text)) return null;
+                return LocalDateTime.parse(text, DateTimeFormatter.ofPattern(DATETIME_PATTERN));
             }
 
+            @Nullable
             @Override
-            public String print(LocalDateTime localDateTime, Locale locale) {
-                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            public String print(@Nullable LocalDateTime localDateTime, @Nullable Locale locale) {
+                if (Objects.isNull(localDateTime)) return null;
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATETIME_PATTERN);
                 return formatter.format(localDateTime);
             }
 
         };
     }
-
 
 
 }
