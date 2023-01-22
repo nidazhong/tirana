ed729a13a0d69541d22883a17c16321d94965b73
nidazhong <nidazhong17@163.com>
2023-01-22 22:54:12 +0800

    update 菜单管理

---
+++ src/main/java/com/ndz/wheat/mini/dao/sys/SysMenuDao.java
@@ -0,0 +1,9 @@
+package com.ndz.wheat.mini.dao.sys;
+
+import com.ndz.wheat.mini.dao.base.BaseDao;
+import com.ndz.wheat.mini.entity.sys.SysMenuEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface SysMenuDao extends BaseDao<SysMenuEntity> {
+}
